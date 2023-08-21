dc08474a7ee2950b88d7239801e8c00c74ba6464
zacariasjuliano <zacariasjuliano@gmail.com>
2023-08-21 13:25:42 +0100

    Atualizações Solições PSSST

--- resources/views/solucoes/tlim.blade.php
+++ resources/views/solucoes/tlim.blade.php
@@ -39,7 +39,7 @@
             width: 475px;
             height: 5%;
             position: absolute;
-            top: 418.8%;
+            top: 484.1%;
             left: 51%;
             border-radius: 12.5% 12.5% 0 0;
             transform: skewY(0deg);
@@ -137,14 +137,13 @@
     </div>
 
     <div style="background-color: rgb(246, 246, 246);">
-
         <div style="padding: 1.5% 20% 6% 20%;">        
             <div class="card-deck mb-3" >
                 <div class="card mb-4 padding" style="background-color: #0f0034; margin-left: 0px; margin-right: -3px; border-radius: 25px 0 0 0;">
                     <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 3%">
                         Artigos
                     </h5> 
-                    <p style="font-size: 15px; color: white; padding: 0 5%">
+                    <p style="font-size: 15px; color: white; padding: 0 5%; line-height: 30px; padding: 0 12%">
                         Com o TlimTM faz uma gestão eficiente de todos os artigos que comercializa no seu estabelecimento. 
                         As reposições são rápidas. O stock otimizado. <br>Cores, tamanhos, dimensões, famílias de artigos, 
                         códigos de barras e outras caraterísticas dos artigos são tratadas de forma intuitiva, para que 
@@ -156,7 +155,7 @@
                     <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 3%">
                         Caixa
                     </h5> 
-                    <p style="font-size: 15px; color: white; padding: 0 5%">
+                    <p style="font-size: 15px; color: white; padding: 0 5%; line-height: 30px; padding: 0 12%">
                         As operações de caixa são muito simples com TlimTM. Um ecrã tátil intuitivo simplifica as operações 
                         de venda ao balcão. Qualquer operador, sem formação, facilmente executa operações como abertura/fecho, 
                         entradas/saídas de caixa, fundo de maneio, diversos tipos de pagamentos, devoluções, trocas, entre 
@@ -167,10 +166,10 @@
         
             <div class="card-deck mb-3" style="margin-bottom: -3rem!important; margin-top: -3rem!important;">
                 <div class="card mb-4 padding" style="background-color: #0f0034; margin-left: 0px; margin-right: -3px; border-radius: 0 0 0 25px;">
-                    <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 0%">
+                    <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 6%">
                         Clientes
                     </h5> 
-                    <p style="font-size: 15px; color: white; padding: 0 5%">
+                    <p style="font-size: 15px; color: white; padding: 0 5%; line-height: 30px; padding: 0 12%">
                         laneie as suas campanhas promocionais e execute-as sem dificuldade. <br>Com o TlimTM é muito simples 
                         definir períodos especiais com preços diferentes para determinados artigos, quantidades de artigos 
                         ou clientes.
@@ -178,22 +177,20 @@
                 </div>
 
                 <div class="card mb-4 padding" style="background-color: #0f0034; margin-left: -3px; margin-right: -3px; border-radius: 0 0 25px 0;">
-                    <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 0%">
+                    <h5 class="text-center" style="font-size: 30px; color: white; padding-top: 6%">
                         Gestão
                     </h5> 
-                    <p style="font-size: 15px; color: white; padding: 0 5%">
+                    <p style="font-size: 15px; color: white; padding: 0 5%; line-height: 30px; padding: 0 12%">
                         Desde a assiduidade dos funcionários, até à avaliação dos resultados das vendas, passando pelo controlo 
                         dos custos operacionais. O TlimTM dá-lhe a informação de que precisa para gerir o negócio de forma eficiente.
                     </p>  
                 </div>                
             </div>            
         
-        </div>
-                
+        </div>                
     </div>  
 
-    <div style="background-color: #8abce6; height: 450px; position: relative; padding: 0% 0%">
-        
+    <div style="background-color: #8abce6; height: 450px; position: relative; padding: 0% 0%">        
         <div class="contanier" style="flex: 1; padding-top: 4%;">
             <h4 style="font-size: 35px; color: black; text-align: -webkit-center;"><b>Impulsione o seu negócio com o Tlim</b></h4> 
 
@@ -205,36 +202,32 @@
                 <li style="padding-top: 1%; font-size: 23px;">Garantias de cumprimento permanente das obrigações legais e fiscais</li>
                 <li style="padding-top: 1%; font-size: 23px;">Suporte técnico garantido</li>
             </ul>
-
-        </div>
-        
+        </div>        
     </div> 
 
-    <div class="container" style="background-color: white; height: 450px; position: relative; padding-top: 5%;">
-        
+    <div class="container" style="background-color: white; height: 540px; position: relative; padding-top: 5%;">        
         <div style="flex: 1; padding-top: 2.5%;">
             <h5 style="font-size: 35px; color: blue;  text-align: -webkit-center;"><b>Conheça As Modalidades De Acesso A Esta Solução.</b></h5> 
-            <p style="font-size: 23px; color: black; text-align: -webkit-left; padding-left: 6%;">
+            <p style="font-size: 23px; color: black; text-align: -webkit-left; padding-left: 9%; padding-right: 9%;">
                 Esta solução está disponível para instalação mediante subscrição. Isto significa que ao subscrever o 
                 produto, acede a uma licença de utilização temporária (anual ou por dois anos).
             </p>  
-            <p style="font-size: 23px; color: black; text-align: -webkit-left; padding-left: 6%;">
+            <p style="font-size: 23px; color: black; text-align: -webkit-left; padding-left: 9%; padding-right: 9%;">
                 A subscrição contempla o acesso ao software, assim como à respetiva atualização e manutenção durante 
                 o período de acesso escolhido. Para aceder à solução basta efetuar a subscrição nos planos a baixo.
             </p>
-        </div>
-        
+        </div>        
     </div> 
 
-    <div style="background-color: #8abce6; height: 980px; position: relative; padding: 0% 0%">
+    <div style="background-color: #8abce6; height: 1150px; position: relative; padding: 0% 0%">
         
         <div style="flex: 1; padding-left: 0%; padding-top: 7%;">
             <p style="font-size: 40px; color: white; text-align: -webkit-center;"><b>Simplifique a gestão de Caixa.</b></p> 
             <p style="font-size: 25px; color: black; text-align: -webkit-center;">
                 Operações rápidas, simples e intuitivas.
             </p>  
-            <p style="font-size: 25px; color: white; text-align: -webkit-left; padding: 0% 20%; word-spacing: 10px;">
-                Aumente a eficiência do seu negócio com a solução de frente de loja Tlim. Organize facilmente a 
+            <p style="font-size: 25px; color: white; text-align: -webkit-left; padding: 0% 22%; word-spacing: 10px;">
+                Aumente a eficiência do seu negócio com a solução de frente de loja Tlim. <br>Organize facilmente a 
                 informação num ecrã tátil, simples e intuitivo. As operações são fáceis e rápidas, promovendo a 
                 máxima produtividade de todos os operadores. 
                 <br><br>Otimize a gestão de operadores, fundo de maneio, aberturas, entradas, saídas e fecho de Caixa. 
@@ -248,24 +241,23 @@
     </div> 
 
 
-    <div style="background-color: #b2b2b2; height: 850px; position: center; padding-top: 6.5%">        
-            
+    <div style="background-color: #b2b2b2; height: 850px; position: center; padding-top: 6.5%">  
         <div style="display: flex; justify-content: center; align-items: center">
 
             <div style="text-align: center; border-radius: 25px; background-color: white; padding-top: 2.5%; margin-right: 1%; width:25%; height:600px">
-                <h5 style="font-size:30px; color: blue;"><b>TLIM EASY</b></h5>
+                <h5 style="font-size:30px; color: blue; padding-top: 4%"><b>TLIM EASY</b></h5>
                 <h5 style="font-size:30px; color: #a4a3a5;"><b>1 ANO</b></h5>
                 <h5 style="font-size:15px; color: black;"><b>PREÇO SOB CONSULTA</b></h5>
 
-                <hr style="width: 100%; color: black; border-top: 1.5px solid black; transform: translateX(-18%); margin-left: 85px;">
+                <hr style="width: 100%; color: black; border-top: 1.5px solid black; margin-left: 0px;">
 
-                <ul style="text-align: left">
-                    <li style="padding-top: 6%">Gestão exclusiva do ponto de venda</li>
+                <ul style="text-align: left; padding-bottom: 11%;">
+                    <li style="padding-top: 6%;">Gestão exclusiva do ponto de venda</li>
                     <li style="padding-top: 6%">Limitado a 1 posto e 2 utilizadores</li>
                     <li style="padding-top: 6%">Atualizações legais e fiscais</li>
                 </ul>
 
-                <hr style="width: 100%; color: black; border-top: 1.5px solid black; transform: translateX(-18%); margin-left: 85px;"> 
+                <hr style="width: 100%; color: black; border-top: 1.5px solid black; margin-left: 0px;"> 
 
                 <div class="botao_2 btn-center  botao_cor">
                     <a href="#" style="color: white;">
@@ -277,10 +269,10 @@
             <div style="text-align: center; border-radius: 25px; background-color: white; padding-top: 5%; margin-left: 1%; width:25%; height:600px">
                 <div class="circle3" style="background-color: green;">
                     <div class="top-part"></div>
-                    <p style="font-size:20px; color: white; padding-top: 4%"><b>RECOMENDADOS</b></p>
+                    <p style="font-size:20px; color: white; padding-top: 1%"><b>RECOMENDADOS</b></p>
                 </div>
 
-                <h5 style="font-size:30px; color: blue;"><b>TLIM</b></h5>
+                <h5 style="font-size:30px; color: blue; margin-top: -5%; margin-bottom: 10%"><b>TLIM</b></h5>
                 <h5 style="font-size:25px; color: #25006d;" hidden><b>2 ANOS</b></h5>
                 <h5 style="font-size:15px; font-style:bond; color: black;"><b>PREÇO SOB CONSULTA</b></h5>
                 
@@ -303,8 +295,6 @@
             </div>
 
         </div>
-            
-        
     </div>  
 
 
